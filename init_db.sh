8ae5290c700dc8ee63c4c53e76f124db2009295d
DP <quyettu347@gmail.com>
2026-02-19 17:07:22 +0700

    chore: refine .gitignore to exclude development artifacts and internal docs

--- init_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Đang khởi tạo cơ sở dữ liệu..."
-docker exec -i mssql_quanlynhanvien /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P 'YourPassword123!' -i /usr/config/CreateDatabase.sql -C
-echo "Xong!"
